ee7df7702661a3ec1ccac5ebbd67daa1c8c2bfde
lordchaz <lordchaz@gmail.com>
2014-01-08 19:10:32 -0800

    Signed-off-by: lordchaz <lordchaz@gmail.com>

---
+++ CC1798220/DevC/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Charles Carsten
+ * Created on January 6, 2014, 6:13 PM
+ * Our first program in DevC++
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here
+int main(int argc, char *argv[]){
+    //Print Hello World
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ CC1798220/netbeans/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Charles Carsten
+ * Created on January 6, 2014, 6:13 PM
+ * Our first program in netbeans
+ */
+
+//System Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Print the string Helloo World
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 0;
+}
+
